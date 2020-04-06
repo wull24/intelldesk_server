c55a3d6b7b51b993bacc4cb78c4174924dd7a76a
wull24 <wull_24@163.com>
2020-04-06 10:29:25 +0800

    Initial commit

---
+++ src/main/java/cn/edu/shu/intelldesk/IntelldeskApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.shu.intelldesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelldeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelldeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/shu/intelldesk/IntelldeskApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.shu.intelldesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelldeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
